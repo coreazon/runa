b9ee9c4f8211afa1bb911b04f45f259b4b6ab630
professorkeinplan <normanmatteo@gmail.com>
2022-03-17 19:00:55 +0100

    added gamelvl, hp and return method for the shuffle.todo: should add types

--- src/model/entitie/mobs/Mobs.java
+++ src/model/entitie/mobs/Mobs.java
@@ -2,31 +2,51 @@ package model.entitie.mobs;
 
 public enum Mobs {
 
-    FROG("Frog"),
-    GHOST("Ghost"),
-    GORGON("Gorgon"),
-    SKELETON("Skeleton"),
-    SPIDER("Spide"),
-    GOBLIN("Goblin"),
-    RAT("Rat"),
-    MUSHROOMLIN("Mushroomlin"),
-    SNAKE("Snake"),
-    DARK_ELF("Dark Elf"),
-    SHADOW_BLADE("Shadow Blade"),
-    HORNET("Hornet"),
-    TARANTULA("Tarantula"),
-    BEAR("Bear"),
-    MUSHROOMLON("Mushroomlon"),
-    WILD_BOAR("Wild Boar");
+    FROG("Frog", 1, new HealthPoints(16)),
+    GHOST("Ghost", 1, new HealthPoints(15)),
+    GORGON("Gorgon", 1, new HealthPoints(13)),
+    SKELETON("Skeleton", 1, new HealthPoints(14)),
+    SPIDER("Spide", 1, new HealthPoints(15)),
+    GOBLIN("Goblin", 1, new HealthPoints(12)),
+    RAT("Rat", 1, new HealthPoints(14)),
+    MUSHROOMLIN("Mushroomlin", 1, new HealthPoints(20)),
+    SNAKE("Snake", 2, new HealthPoints(31)),
+    DARK_ELF("Dark Elf", 2, new HealthPoints(34)),
+    SHADOW_BLADE("Shadow Blade", 2, new HealthPoints(27)),
+    HORNET("Hornet", 2, new HealthPoints(32)),
+    TARANTULA("Tarantula", 2, new HealthPoints(33)),
+    BEAR("Bear", 2, new HealthPoints(40)),
+    MUSHROOMLON("Mushroomlon", 2, new HealthPoints(50)),
+    WILD_BOAR("Wild Boar", 2, new HealthPoints(27));
 
     private final String representation;
+    private final int gameLevel;
+    private final HealthPoints healthPoints;
 
 
-    Mobs(String representation) {
+    Mobs(String representation, int gameLevel, HealthPoints healthPoints) {
         this.representation = representation;
+        this.gameLevel = gameLevel;
+        this.healthPoints = healthPoints;
     }
 
     public String getRepresentation() {
         return representation;
+    }
+
+    public int getGameLevel() {
+        return gameLevel;
+    }
+
+    public HealthPoints getHealthPoints() {
+        return healthPoints;
+    }
+
+    public static List<Mobs> getMobsFromGameLevel(int gameLevel) {
+        var mobs = new ArrayList<Mobs>();
+        for (Mobs mob : Mobs.values()) {
+            if (mob.getGameLevel() == gameLevel) mobs.add(mob);
+        }
+        return mobs;
     }
 }
