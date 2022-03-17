4e61786ac5008f8b305e699fe000e69c9756aea2
professorkeinplan <normanmatteo@gmail.com>
2022-03-17 18:59:40 +0100

    added hp, gamelevel and type to bossmobs

--- src/model/entitie/mobs/BossMobs.java
+++ src/model/entitie/mobs/BossMobs.java
@@ -2,16 +2,35 @@ package model.entitie.mobs;
 
 public enum BossMobs {
 
-    SPIDER_KING("Spider King"),
-    MEGA_SAURUS("Mega Saurus");
+    SPIDER_KING("Spider King", 1, new HealthPoints(50), Type.LIGHTNING),
+    MEGA_SAURUS("Mega Saurus", 2, new HealthPoints(100), Type.NONE);
 
     private final String representation;
+    private final int gameLevel;
+    private final HealthPoints healthPoints;
+    private final Type type;
 
-    BossMobs(String representation) {
+    BossMobs(String representation, int gameLevel, HealthPoints healthPoints, Type type) {
         this.representation = representation;
+        this.gameLevel = gameLevel;
+        this.healthPoints = healthPoints;
+        this.type = type;
+    }
+
+    public Type getType() {
+        return type;
     }
 
     public String getRepresentation() {
         return representation;
     }
+
+    public int getGameLevel() {
+        return gameLevel;
+    }
+
+    public HealthPoints getHealthPoints() {
+        return healthPoints;
+    }
+
 }
